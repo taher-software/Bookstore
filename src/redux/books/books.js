a73d09363c9b5b528c2b79df6d5c6652781ee1a0
Taher <ttaherhagui@gmail.com>
2021-12-07 12:08:27 +0100

    Add book reducer and configure store.

---
+++ src/redux/books/books.js
@@ -0,0 +1,28 @@
+const ADD_BOOK = 'bookStore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+
+export const addBook = payload => ({
+    type: ADD_BOOK,
+    payload : payload
+});
+
+export const removeBook = payload => ({
+    type: REMOVE_BOOK,
+    payload : payload
+})
+
+const booksReducer = (state = [], action) => {
+    switch (action.type){
+      case ADD_BOOK:
+          return [
+            ...state,
+            action.payload
+          ];
+      case REMOVE_BOOK:
+          return state.filter(book => book.id !== action.payload);
+      default :
+          return state;
+    }
+}
+
+export default booksReducer;
