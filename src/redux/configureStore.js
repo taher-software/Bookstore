97f9e4ca93344acfbbc794c219f2a9978bd448ea
Taher <ttaherhagui@gmail.com>
2021-12-08 16:57:10 +0100

    configurate redux thunk.

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import booksReducer from './books/books';
 
 const reducer = combineReducers({ booksReducer });
 const store = createStore(
   reducer,
   applyMiddleware(logger),
+  applyMiddleware(thunk)
 );
 export default store;
